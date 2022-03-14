03d69d90711bb9ccfa6706225619f4bde1e71e21
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-03-13 22:04:16 -0400

    add account modify

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ const host = window.location.host
 const displayAccountBtn = document.querySelector("#displayAccountBtn")
 const deleteAccountBtn = document.querySelector("#deleteAccountBtn")
 const logOutBtn = document.querySelector("#logOutBtn")
+const modifyAccountModalSaveButton = document.querySelector("#modifyAccountModalSaveButton")
 
 // display account
 displayAccountBtn.addEventListener("click", async(e) => {
@@ -90,4 +91,44 @@ logOutBtn.addEventListener("click", async(e) => {
     } else {
         console.log("HTTP-Error: " + response.status)
     }
+})
+
+// modify account
+modifyAccountModalSaveButton.addEventListener("click", async(e) => {
+    e.preventDefault()
+
+    const token = localStorage.getItem("token")
+
+    //const url = "http://localhost:3001/users/me"
+    const url = 'https://sethspire-api.herokuapp.com/users/me'
+
+    const nameInput = document.querySelector("#nameInput")
+    const passwordInput = document.querySelector("#passwordInput")
+    const name = nameInput.value
+    const password = passwordInput.value
+    const requestData = {...name && { name }, ...password && { password } }
+    //console.log(requestData)
+
+    const options = {
+        method: "PATCH",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+    }
+
+    let response = await fetch(url, options)
+
+    if (response.status === 200) {
+        const contentArea = document.querySelector("#accountDisplayArea")
+        contentArea.innerHTML = `Change successful.`
+    } else {
+        console.log("HTTP-Error: " + response.status)
+    }
+
+    const modal = document.querySelector("#modifyAccountModal")
+    bootstrap.Modal.getInstance(modal).hide()
+
+    const form = document.querySelector("#modifyAccountForm").reset()
 })
